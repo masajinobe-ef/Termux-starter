cbbe2060516b2b7aeadc08f49ceb3eedc9303234
Kolesov Arseny <90415866+masajinobe-ef@users.noreply.github.com>
2024-09-09 14:25:50 +0500

    Update starter.sh

--- starter.sh
+++ starter.sh
@@ -5,8 +5,6 @@ pkg install git tsu cronie termux-services -y
 
 termux-setup-storage
 
-sv-enable crond
-
 git config --global credential.helper store
 git config --global user.email "priscilla.effects@gmail.com"
 git config --global user.name "masajinobe-ef"
